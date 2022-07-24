30f68d5657fb80be395002c8e6dcef08706bba82
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2022-07-24 12:59:55 +0700

    Update checkmask.ino

--- checkmask.ino
+++ checkmask.ino
@@ -1,1 +1,179 @@
+#include "TridentTD_LineNotify.h"
+#include "esp_camera.h"
+#include "fd_forward.h"
 
+#define LINE_TOKEN "YOUR TOKEN HERE"
+#define ssid "ssid"
+#define password "password"
+
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+static inline mtmn_config_t app_mtmn_config()
+{
+    mtmn_config.t mtmn_config = {0};
+    mtmn_config.type = FAST;
+    mtmn_config.min_face = 80;
+    mtmn_config.pyramid = 0.707;
+    mtmn_config.pyramid_times = 4;
+    mtmn_config.p_threshold.score = 0.6;
+    mtmn_config.p_threshold.nms = 0.7;
+    mtmn_config.p_threshold.candidate_number = 20;
+    mtmn_config.r_threshold.score = 0.7;
+    mtmn_config.r_threshold.nms = 0.7;
+    mtmn_config.r_threshold.candidate_number = 10;
+    mtmn_config.o_threshold.score = 0.7;
+    mtmn_config.o_threshold.nms = 0.7;
+    mtmn_config.o_threshold.candidate_number = 1;
+    return mtmn_config;
+}
+
+mtmn_config_t mtmn_config = app_mtmn_config();
+
+unsigned long currentTime = millis();
+unsigned long oldTime = currentTime;
+
+bool sendStatus = false;
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println();
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+    if (psramFound())
+    {
+        config.frame_size = FRAMESIZE_UXGA;
+        config.jpeg_quality = 10;
+        config.fb_count = 2;
+    }
+    else
+    {
+        config.frame_size = FRAMESIZE_SVGA;
+        config.jpeg_quality = 12;
+        config.fb_count = 1;
+    }
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK)
+    {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        ESP.restart();
+    }
+    sensor_t *s = esp_camera_sensor_get();
+    s->set_framesize(s, FRAMESIZE_QVGA);
+    int connectCount = 0;
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        connectCount++;
+        delay(500);
+        Serial.print(".");
+        if (connectCount > 10)
+        {
+            Serial.println("WiFi connect failed");
+            ESP.restart();
+        }
+    }
+    LINE.setToken(LINE_TOKEN);
+}
+
+void loop()
+{
+    currentTime = millis();
+    if (currentTime - oldTime > 100)
+    {
+        detectFace();
+    }
+}
+
+void detectFace()
+{
+    camera_fb_t *fb = NULL;
+    dl_matrix3du_t *image_matrix = NULL;
+    while (true)
+    {
+        fb = esp_camera_fb_get();
+        if (!fb)
+        {
+            Serial.println("camera broken lol");
+        }
+        else
+        {
+            image_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 1);
+            if (!image_matrix)
+            {
+                Serial.println("mem alloc brokey");
+            }
+            else
+            {
+                if (!fmt2rgb888(fb->buf, fb->len, fb->format, image_matrix->item))
+                {
+                    Serial.println("fmt2rgb888 broken lol");
+                }
+                else
+                {
+                    box_array_t *net_boxes = NULL;
+                    net_boxes = face_detect(image_matrix, &mtmn_config);
+                    if (net_boxes)
+                    {
+                        if ((float)net_boxes->score[0] > 0.99)
+                        {
+                            Serial.println("why no mask bruh");
+                            LINE.notifyPicture("มีคนไม่ใส่หน้ากากอนามัยโว้ย!", fb->buf, fb->len);
+                            Serial.println("pic sent");
+                            delay(500);
+                            sendStatus = true;
+                        }
+                        // lemme free some mems
+                        free(net_boxes->score);
+                        free(net_boxes->box);
+                        free(net_boxes->landmark);
+                        free(net_boxes);
+                    }
+                    // free dat buffer
+                    esp_camera_fb_return(fb);
+                    fb = NULL;
+                }
+                dl_matrix3du_free(image_matrix);
+            }
+        }
+        if (sendStatus == true || digitalRead(IntIR1Pin) == 1)
+        {
+            sendStatus = false;
+            break;
+        }
+    }
+}
